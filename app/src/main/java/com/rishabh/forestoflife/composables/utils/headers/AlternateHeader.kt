ed6049e67b5efc3a0ebaba357284c281968582aa
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2023-07-10 13:40:26 +0530

    header start

--- app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/AlternateHeader.kt
+++ app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/AlternateHeader.kt
@@ -1,15 +1,23 @@
 package com.rishabh.forestoflife.composables.utils.headers
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun AlternateHeader(){
+fun AlternateHeader(pageName : String){
     // TODO:: Create Alternate Header that goes on Task List and Create
+    Row() {
+        // Back Button
+        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+
+        Text(text = pageName)
+    }
 }
 
 @Preview
 @Composable
 fun AlternateHeaderPreview(){
-    AlternateHeader()
+    AlternateHeader("Testing")
 }
--- app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/MainHeader.kt
+++ app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/MainHeader.kt
@@ -1,16 +1,33 @@
 package com.rishabh.forestoflife.composables.utils.headers
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
-fun MainHeader(){
+fun MainHeader(pageName: String){
     //TODO: Create Main Header which goes in home, focus, island, profile
+    Row() {
+        Text(text = pageName)
+
+        // Trees in island
+        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+
+        // Water
+        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+
+        // Fertilizer
+        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+    }
 }
 
 @Preview
 @Composable
 fun MainHeaderPreview(){
-    MainHeader()
+    MainHeader("Testing")
 }
