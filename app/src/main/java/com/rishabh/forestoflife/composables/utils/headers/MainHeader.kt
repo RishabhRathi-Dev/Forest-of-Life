03ea5c7511b8fc0368e21eb93803116040341437
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2023-07-10 15:32:47 +0530

    main header

--- app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/MainHeader.kt
+++ app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/MainHeader.kt
@@ -1,33 +1,107 @@
 package com.rishabh.forestoflife.composables.utils.headers
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rishabh.forestoflife.R
 
 
 @Composable
-fun MainHeader(pageName: String){
+fun MainHeader(pageName: String, treesCount: Int, waterCount:Int, fertilizerCount:Int){
     //TODO: Create Main Header which goes in home, focus, island, profile
-    Row() {
-        Text(text = pageName)
+    Row(
+        modifier = Modifier
+            .size(
+                width = LocalConfiguration.current.screenWidthDp.dp, height = 70.dp
+            )
+            .safeContentPadding()
+            .background(color = MaterialTheme.colorScheme.background)
+        ,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = pageName,
+            fontSize = 32.sp,
+            fontFamily = FontFamily(Font(R.font.itim)),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(10.dp)
+        )
 
-        // Trees in island
-        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            // Trees in island
+            Box(modifier = Modifier
+                .wrapContentSize()
+                .padding(5.dp)
+                //.paint(painterResource(id = ), contentScale = ContentScale.FillBounds)
+            ){
+                Text(
+                    text = treesCount.toString(),
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.itim))
+                )
+            }
+            //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
 
-        // Water
-        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+            // Water
+            Box(modifier = Modifier
+                .wrapContentSize()
+                .padding(5.dp)
+                //.paint(painterResource(id = ), contentScale = ContentScale.FillBounds)
+            ){
+                Text(
+                    text = waterCount.toString(),
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.itim))
+                )
+            }
 
-        // Fertilizer
-        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+            // Fertilizer
+            Box(modifier = Modifier
+                .wrapContentSize()
+                .padding(5.dp)
+                //.paint(painterResource(id = ), contentScale = ContentScale.FillBounds)
+            ){
+                Text(
+                    text = fertilizerCount.toString(),
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.itim))
+                )
+            }
+        }
+
     }
 }
 
 @Preview
 @Composable
 fun MainHeaderPreview(){
-    MainHeader("Testing")
+    MainHeader("Testing", 10, 100, 10)
 }
