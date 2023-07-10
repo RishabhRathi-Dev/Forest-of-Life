49a7d9a5cf4edd364a06a6ae495c5a1573aaf201
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2023-07-10 15:06:32 +0530

    alternate header

--- app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/AlternateHeader.kt
+++ app/src/main/java/com/rishabh/forestoflife/composables/utils/headers/AlternateHeader.kt
@@ -1,18 +1,70 @@
 package com.rishabh.forestoflife.composables.utils.headers
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rishabh.forestoflife.R
 
 @Composable
 fun AlternateHeader(pageName : String){
     // TODO:: Create Alternate Header that goes on Task List and Create
-    Row() {
+    Row(
+        modifier = Modifier
+            .size(
+                width = LocalConfiguration.current.screenWidthDp.dp, height = 70.dp
+            )
+            .safeContentPadding()
+            .background(color = MaterialTheme.colorScheme.background)
+        ,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         // Back Button
-        //Icon(painter = painterResource(id = R.drawable), contentDescription = "Test")
+        Button(
+            modifier = Modifier
+                .wrapContentSize()
+                .offset(x=(-15).dp)
+            ,
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.background, contentColor = Color.Black),
+            onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.arrow_back_48px),
+                contentDescription = "Test",
+            )
+        }
 
-        Text(text = pageName)
+        Text(
+            text = pageName,
+            fontSize = 32.sp,
+            fontFamily = FontFamily(Font(R.font.itim)),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .size(width = LocalConfiguration.current.screenWidthDp.dp, height = 70.dp)
+                .offset(x = (-40).dp, y = 10.dp)
+        )
     }
 }
 
--- app/src/main/java/com/rishabh/forestoflife/ui/theme/Type.kt
+++ app/src/main/java/com/rishabh/forestoflife/ui/theme/Type.kt
@@ -2,14 +2,16 @@ package com.rishabh.forestoflife.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rishabh.forestoflife.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.itim)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
